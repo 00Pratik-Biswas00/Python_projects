655be48df2a1c10b6acf90f9834447d91a522636
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-06 09:24:39 +0530

    Update 2. Password_generator.py

--- 2. Password_generator.py
+++ 2. Password_generator.py
@@ -11,16 +11,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like in your password?\n"))
 nr_numbers = int(input(f"How many numbers would you like in your password?\n"))
 
-# password = ""
-# for p in range(1, nr_letters + 1):
-#     password += random.choice(letters)
-# for p in range(1, nr_symbols + 1):
-#     password += random.choice(symbols)
-# for p in range(1, nr_numbers + 1):
-#     password += random.choice(numbers)
-#
-# print(password)
-
 blank_password = []
 
 for l in range(1, nr_letters + 1):
