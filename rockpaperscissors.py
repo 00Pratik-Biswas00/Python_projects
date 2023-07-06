04d46a5137689efe05ccd9819e156f71821201e0
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-06 08:28:01 +0530

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,56 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+choice=''
+while choice.lower() != 'no':
+    com = random.randint(0, 2)
+    a = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+    if a == com:
+        print("You both choose the same. Match drawn")
+    elif (a == 0) and (com == 1):
+        print( "You:" + rock + "\n" + "Computer" + paper + "\n" + "Computer wins.")
+    elif (a == 1) and (com == 0):
+        print( "You:" + paper + "\n" + "Computer" + rock + "\n" + "You win.")
+    elif (a == 0) and (com == 2):
+        print( "You:" + rock + "\n" + "Computer" + scissors + "\n" + "You win.")
+    elif (a == 2) and (com == 0):
+        print( "You:" + scissors + "\n" + "Computer" + rock + "\n" + "Computer wins.")
+    elif (a == 1) and (com == 2):
+        print( "You:" + paper + "\n" + "Computer" + scissors + "\n" + "Computer wins.")
+    elif (a == 2) and (com == 1):
+        print( "You:" + scissors + "\n" + "Computer" + paper + "\n" + "You win.")
+    else:
+        print("invalid number")
+    choice = input("Do you want to play again? Yes or No? -> ").lower()
+
+
+
+
+
