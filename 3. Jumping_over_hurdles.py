db5470991b4d8f23ca66a5d78dd318cd1727de2c
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-06 10:17:11 +0530

    Update 3. Jumping_over_hurdles.py

--- 3. Jumping_over_hurdles.py
+++ 3. Jumping_over_hurdles.py
@@ -1,18 +1,18 @@
-# It is a Reedborg robot hurdle game, where many inbuilt functions are present like turn_left(), right_is_clear() etc. The details of the 
-# inbuilt functions are present in the Reedborg' keywords section. The Reedborg website link is attached in the Readme file under number 3.
+# It is a Reedborg robot hurdle game with many inbuilt functions, like turn_left(), right_is_clear(), etc. The details of the 
+# inbuilt functions are present in Reedborg's keywords section. The Reedborg website link is attached in the Readme file under number 3.
 
-def right():
+def right():    # Function to turn right
     turn_left()
     turn_left()
     turn_left()
 
-def height():
+def height():    # Function to measure the height of a wall
     if right_is_clear():
         move()
     else:
         turn_left()
 
-while not at_goal():
+while not at_goal():    
     if right_is_clear():
         right()
         move()
