ec4e6ec5b78141101ea6446e567ff18abe6e51e4
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-07 10:30:12 +0530

    Update 5. Encode_decode_messages.py

--- 5. Encode_decode_messages.py
+++ 5. Encode_decode_messages.py
@@ -23,12 +23,12 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    if cipher_direction.lower() == "decode":
+    if cipher_direction.lower() == "decode":    # For decoding
         shift_amount *= -1
-    elif cipher_direction.lower() != "encode":
+    elif cipher_direction.lower() != "encode":    # For encoding
         print("\nWrong operation!")
         return
-    for char in start_text:
+    for char in start_text:    
         if char in alphabet:
             position = alphabet.index(char)
             new_position = position + shift_amount
@@ -38,14 +38,13 @@ def caesar(start_text, shift_amount, cipher_direction):
 
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
-
 should_run = True
 
 while should_run:
     direction = input("Type Encode to encrypt, type Decode to decrypt: ")
     text = input("\nType your message: ").lower()
     shift = int(input("\nType the shift number: "))
-    shift = shift % 26
+    shift = shift % 26    # Use modulus operation to traverse the alphabet list circularly
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
     wish_to_contd = input("\nType 'yes' to continue and 'no' to terminate: ")
     if wish_to_contd == "no":
