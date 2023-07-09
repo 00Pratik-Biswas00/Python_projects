4ac3a44f74d417d2f8f1e4aa94cf4268f09b2171
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-09 12:37:36 +0530

    Update Quiz_brain.py

--- C. True_false_quiz_game/Quiz_brain.py
+++ C. True_false_quiz_game/Quiz_brain.py
@@ -7,21 +7,16 @@ class QuizBrain:
         self.score = 0
         self.question_list = q_list
 
-    # def still_have_any_question(self):
-    #     return self.question_number < 5
-
-    def next_question(self):
+    def next_question(self):    # Generates next question
         current_question = self.question_list[random.randint(0, len(self.question_list) - 1)]
         self.question_number += 1
         user_answer = input(f"\nQ.{self.question_number}: {current_question.text} (True/False)? -> ").lower()
         self.check_answer(user_answer, current_question.answer)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer, correct_answer):    # Checks the result 
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
             print("\nYou're right!!")
         else:
             print("\nYou're wrong.")
         print(f"The correct answer is: {correct_answer}\nYour score is: {self.score}/{self.question_number}\n")
-
-# random.randint(0, len(self.question_list))
