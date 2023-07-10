9332a8e3088867f90efbf3fad3c25fb882150d1c
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-07-10 11:21:05 +0530

    Update Different_geometric_shapes.py

--- D. Play_with_turtle/Different_geometric_shapes.py
+++ D. Play_with_turtle/Different_geometric_shapes.py
@@ -3,20 +3,20 @@ import turtle as t
 
 turtle = t.Turtle()
 turtle.shape("arrow")
-turtle.penup()
+turtle.penup()    # Change the position
 turtle.goto(-50, 200)
 turtle.pendown()
 color_name = ["cyan", "spring green", "tomato", "saddle brown", "dark violet"]
 
 
-def draw_shapes(slide_number):
-    angle = 360 / slide_number
-    for i in range(slide_number):
+def draw_shapes(side_number):
+    angle = 360 / side_number
+    for i in range(side_number):
         turtle.forward(100)
         turtle.right(angle)
 
 
-for number_of_side in range(3, 13):
+for number_of_side in range(3, 13):		# Different side number give different shapes
     turtle.color(random.choice(color_name))
     draw_shapes(number_of_side)
 
