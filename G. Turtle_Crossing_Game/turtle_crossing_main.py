01356c72f38e4cd0d767bb1ae598d86512119163
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2023-12-31 12:47:27 +0530

    Add files via upload

---
+++ G. Turtle_Crossing_Game/turtle_crossing_main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # Detect collision with the car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect when the player reaches to the other side
+    if player.reached_destination():
+        player.start_again()
+        car_manager.increase_car_speed()
+        scoreboard.increase_level()
+
+screen.exitonclick()
+
+
