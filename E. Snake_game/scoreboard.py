c095fe34b13009a7a6dae785c8334481b1c97677
Pratik Biswas <114896796+00Pratik-Biswas00@users.noreply.github.com>
2024-01-01 11:51:54 +0530

    Update scoreboard.py

--- E. Snake_game/scoreboard.py
+++ E. Snake_game/scoreboard.py
@@ -6,19 +6,35 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 280)
-        self.update_scoreboard()
         self.hideturtle()
+        self.update_scoreboard()
+
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 15, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align="center", font=("Courier", 15, "normal"))
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"Game Over. Your final score is: {self.score}", align="center", font=("Courier", 18, "normal"))
+    def reset_score(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.clear()
+        self.goto(0, 0)
+        self.write(f"Game Over. Your final score is: {self.score}\n\t  High Score: {self.high_score}", align="center",
+                   font=("Courier", 18, "normal"))
+        self.score = 0
+        # self.update_scoreboard()
+
+    # def game_over(self):
+    #     # self.clear()
+    #     self.goto(0,0)
+    #     self.write(f"Game Over. Your final score is: {self.score}\n\t  High Score: {self.high_score}", align="center", font=("Courier", 18, "normal"))
 
     def increase_score(self):
         self.score+=1
-        self.clear()
         self.update_scoreboard()
